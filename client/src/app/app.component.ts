957a9f2bb3d74b4f292abb500ba9fa2abdc1af45
Reza Taba <mrtabaa@gmail.com>
2023-10-14 04:44:57 -0700

    showUsers() + service + material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+// import { User } from './models/user.model';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  // users: User[] | undefined;
+
+  constructor(private http: HttpClient, private accountService: AccountService, private fb: FormBuilder) {
+
+  }
+
+  // ngOnInit(): void {
+    // this.registerFg;
+  // }
+
+  // showUsers(): void {
+    // this.http.get<User[]>('https://localhost:5001/api/use').subscribe({
+    //   next: (response: User[]) => {
+    //     console.log(response);
+    //     this.users = response;
+    //   },
+    //   error: err => console.log(err.message)
+    // });
+
+    // this.accountService.getUsers().subscribe({
+    //   next: (response: User[]) => {
+    //     console.log(response);
+    //     this.users = response;
+    //   }
+    // });
+  // }
+
+
+  // registerFg = this.fb.group({
+  //   emailCtrl: ['', [Validators.required, Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/)]],
+  //   passwordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
+  //   confirmPasswordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
+  // });
+
+  // get EmailCtrl(): FormControl {
+  //   return this.registerFg.get('emailCtrl') as FormControl;
+  // }
+  // get PasswordCtrl(): FormControl {
+  //   return this.registerFg.get('passwordCtrl') as FormControl;
+  // }
+  // get ConfirmPassword(): FormControl {
+  //   return this.registerFg.get('confirmPasswordCtrl') as FormControl;
+  // }
+
+  // registerUser(): void {
+  //   let user: User = {
+  //     email: this.EmailCtrl.value,
+  //     password: this.PasswordCtrl.value,
+  //     confirmPassword: this.ConfirmPassword.value
+  //   }
+
+  //   this.http.post('https://localhost:5001/api/account/register', user).subscribe({
+  //     next: user => {
+  //       console.log(user);
+  //     },
+  //     error: err => console.log(err)
+  //   });
+  // }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// // CRUD
+// import { HttpClientModule } from '@angular/common/http';
+
+// // Form
+// import { FormsModule } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
+
+// // Material
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // // CRUD
+    // HttpClientModule,
+
+    // // Form
+    // FormsModule,
+    // ReactiveFormsModule,
+
+    // // Material
+    // MatFormFieldModule,
+    // MatInputModule,
+    // MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
