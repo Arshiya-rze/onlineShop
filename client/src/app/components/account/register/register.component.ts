dda3c3c07bbd80cf78c3465a9e1405e9166cf7a5
Reza Taba <mrtabaa@gmail.com>
2023-10-21 22:20:08 +0330

    client => accountService => register

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegisterUser } from 'src/app/models/register-user.model';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private accountService: AccountService) {}
+
+  register(): void {
+    let userInput: RegisterUser = {
+      email: 'a4@a.com',
+      password: 'aaaaaaa',
+      confirmPassword: 'aaaaaaa'
+    }
+
+    this.accountService.registerUser(userInput).subscribe({
+      next: user => console.log(user)
+    })
+  }
 }
