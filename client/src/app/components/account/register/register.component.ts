b44fe4f3fe01aeb528b8fb33a1b6c5e96b149b0f
Reza Taba <mrtabaa@gmail.com>
2023-10-22 22:06:45 +0330

    add register user in client

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { RegisterUser } from 'src/app/models/register-user.model';
 import { AccountService } from 'src/app/services/account.service';
 
@@ -9,16 +10,23 @@ import { AccountService } from 'src/app/services/account.service';
 })
 export class RegisterComponent {
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private fb: FormBuilder) {}
+
+  registerFg = this.fb.group({
+    emailCtrl: ['', [Validators.required, Validators.pattern('^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$')]],
+    passwordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
+    confirmPasswordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]]
+  })
 
   register(): void {
-    let userInput: RegisterUser = {
-      email: 'a4@a.com',
-      password: 'aaaaaaa',
-      confirmPassword: 'aaaaaaa'
-    }
 
-    this.accountService.registerUser(userInput).subscribe({
+    // let user: RegisterUser = {
+    //   email: EmailCtrl.value,
+    //   password: 'aaaaaaa',
+    //   confirmPassword: 'aaaaaaa'
+    // }
+
+    this.accountService.registerUser(user).subscribe({
       next: user => console.log(user)
     })
   }
