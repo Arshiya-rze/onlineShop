b4d38361b33041531ead189812eaa6b1c5005fd5
Reza Taba <mrtabaa@gmail.com>
2023-10-27 12:04:20 -0700

    added loginUser method

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { User } from '../models/user.model';
 import { RegisterUser } from '../models/register-user.model';
+import { LoginUser } from '../models/login-user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,12 @@ export class AccountService {
 
   registerUser(userInput: RegisterUser): Observable<User> {
     return this.http.post<User>('https://localhost:5001/api/account/register', userInput).pipe(
+      map(user => user)
+    );
+  }
+
+  loginUser(userInput: LoginUser): Observable<User>{
+    return this.http.post<User>('https://localhost:5001/api/account/login', userInput).pipe(
       map(user => user)
     );
   }
