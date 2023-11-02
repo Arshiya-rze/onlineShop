05d25417c7b3ca371d3939f95420186eda198d4f
Reza Taba <mrtabaa@gmail.com>
2023-11-02 09:27:01 -0700

    removed noise & teach LocalStorage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,68 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './models/user.model';
-import { UserService } from './services/user.service';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // users: User[] | undefined;
 
   allUsers: User[] | undefined;
-  constructor(private userService: UserService) { }
+  constructor(private accountService: AccountService) { }
 
-  // ngOnInit(): void {
-  // this.registerFg;
-  // }
+  ngOnInit(): void {
+    // this.getLocalStorageCurrentValues();
+  }
 
-  // showUsers(): void {
-  // this.http.get<User[]>('https://localhost:5001/api/use').subscribe({
-  //   next: (response: User[]) => {
-  //     console.log(response);
-  //     this.users = response;
-  //   },
-  //   error: err => console.log(err.message)
-  // });
+  // getLocalStorageCurrentValues(): void {
+  //   const userString: string | null = localStorage.getItem('user');
 
-  // this.accountService.getUsers().subscribe({
-  //   next: (response: User[]) => {
-  //     console.log(response);
-  //     this.users = response;
+  //   if (userString) {
+  //     const user: User = JSON.parse(userString); // convert string to JSON before sending to method
+
+  //     this.accountService.setCurrentUser(user);
   //   }
-  // });
-  // }
-
-
-  // registerFg = this.fb.group({
-  //   emailCtrl: ['', [Validators.required, Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/)]],
-  //   passwordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
-  //   confirmPasswordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
-  // });
-
-  // get EmailCtrl(): FormControl {
-  //   return this.registerFg.get('emailCtrl') as FormControl;
-  // }
-  // get PasswordCtrl(): FormControl {
-  //   return this.registerFg.get('passwordCtrl') as FormControl;
-  // }
-  // get ConfirmPassword(): FormControl {
-  //   return this.registerFg.get('confirmPasswordCtrl') as FormControl;
-  // }
-
-  // registerUser(): void {
-  //   let user: User = {
-  //     email: this.EmailCtrl.value,
-  //     password: this.PasswordCtrl.value,
-  //     confirmPassword: this.ConfirmPassword.value
-  //   }
-
-  //   this.http.post('https://localhost:5001/api/account/register', user).subscribe({
-  //     next: user => {
-  //       console.log(user);
-  //     },
-  //     error: err => console.log(err)
-  //   });
   // }
 }
