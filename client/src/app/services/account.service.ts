f2e9e87c9aaddd59a1cd10b723e69b1c6d3d45a6
Reza Taba <mrtabaa@gmail.com>
2023-11-02 09:29:07 -0700

    teach localStorage

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -10,25 +10,52 @@ import { LoginUser } from '../models/login-user.model';
 })
 export class AccountService {
   private currentUserSource = new BehaviorSubject<User | null>(null);
-  currentUser$ = this.currentUserSource.asObservable(); 
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   registerUser(userInput: RegisterUser): Observable<User> {
     return this.http.post<User>('https://localhost:5001/api/account/register', userInput).pipe(
       map(userResponse => {
-        this.currentUserSource.next(userResponse);
-        return userResponse;
+        // if (userResponse) {
+          this.currentUserSource.next(userResponse);
+          // this.setCurrentUser(userResponse);
+
+          return userResponse;
+        // }
+
+        // return null;
       })
     );
   }
 
-  loginUser(userInput: LoginUser): Observable<User>{
+  loginUser(userInput: LoginUser): Observable<User> {
     return this.http.post<User>('https://localhost:5001/api/account/login', userInput).pipe(
       map(userResponse => {
-        this.currentUserSource.next(userResponse);
-        return userResponse;
+        // if (userResponse) {
+          this.currentUserSource.next(userResponse);
+          // this.setCurrentUser(userResponse);
+
+          return userResponse;
+        // }
+
+        // return null;
       })
     );
   }
+
+  // setCurrentUser(user: User): void {
+  //   localStorage.setItem('user', JSON.stringify(user));
+
+  //   this.currentUserSource.next(user);
+  // }
+
+
+  // logoutUser(): void {
+  //   localStorage.removeItem('user');
+
+  //   this.currentUserSource.next(null);
+  //   this.router.navigate(['/login'])
+  // }
+
 }
