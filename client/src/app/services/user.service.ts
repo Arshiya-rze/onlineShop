c661b5354177261ece9f1aa63e604daf273bb818
Reza Taba <mrtabaa@gmail.com>
2023-11-10 18:14:16 +0330

    add jwt bearer to requests' headers

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,17 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { Observable, delay, map, take } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   // Observable / Promise
   getAllUsers(): Observable<User[]> {
+    // let requestOptions; // Type is not declared since options can vary. see this page
+    // // https://angular.io/api/common/http/HttpClient
+
+    // this.accountService.currentUser$.pipe(take(1)).subscribe({
+    //   next: (user: User | null) => {
+    //     if (user) {
+    //       requestOptions = {
+    //         headers: new HttpHeaders({ 'Authorization': `Bearer ${user.token}` })
+    //       }
+    //     }
+    //   }
+    // });
+
     return this.http.get<User[]>('https://localhost:5001/api/user').pipe(
       map(users => {
         return users;
